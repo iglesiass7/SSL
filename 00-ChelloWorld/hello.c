5b422c0aea345af269f835dc4e7a4d4687cfca87
iglesiass7 <matedelrojo@gmail.com>
2025-04-23 15:51:45 -0300

    Corrijo hello.c para escribir en stdout con printf

--- 00-ChelloWorld/hello.c
+++ 00-ChelloWorld/hello.c
@@ -2,17 +2,6 @@
 
 int main(void)
 {
-<<<<<<< HEAD
-    FILE *archivo;
-    archivo = fopen("output.txt", "w");
-    if (archivo == NULL) {
-        printf("Error al abrir el archivo.\n");
-        return 1;
-    }
-    fprintf(archivo, "Hola Mateo Iglesias!");
-    fclose(archivo);
-=======
->>>>>>> d637e8a (Corrijo hello.c para escribir en stdout con printf)
     printf("Hola Mateo Iglesias!");
     return 0;
 }
